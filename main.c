16748e4fe6123eadc2a0de37c2504a0ce2d172ad
Fabian Lang <fabian.lang@fau.de>
2019-11-12 12:08:38 +0100

    logic for opening source file
    * get filename from command line
    * open file
    * get ELF representation from libelf
    * tidy up when you are done

--- main.c
+++ main.c
@@ -1,5 +1,36 @@
+#include <error.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "libelf/libelf.h"
 
 int main(int argc, char **argv) {
+	// TODO: better command line parsing
+	if (argc != 2)
+		error(EXIT_FAILURE, 0, "usage: %s file-name", argv[0]);
+
+	char *filename = argv[1];
+
+	// libelf-library won't work if you don't tell it the ELF version
+	if (elf_version(EV_CURRENT) == EV_NONE)
+		error(EXIT_FAILURE, 0, "ELF library initialization failed: %s", elf_errmsg(-1));
+
+	int srcfd;	// file descriptor of source file
+	if ((srcfd = open(filename, O_RDONLY)) < 0)
+		error(EXIT_FAILURE, errno, "open %s failed", filename);
+
+	Elf *srce;	// ELF representation of source file
+	if ((srce = elf_begin(srcfd, ELF_C_READ, NULL)) == NULL)
+		// TODO: close file descriptor
+		error(EXIT_FAILURE, 0, "elf_begin() failed: %s", elf_errmsg(-1));
+
+	elf_end(srce);
+	if (close(srcfd) < 0)
+		error(EXIT_FAILURE, errno, "close %s failed", filename);
+
 	return 0;
 }
