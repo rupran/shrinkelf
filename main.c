b34f0a0c480d9ae9edfec84545b912da0d208acf
Fabian Lang <fabian.lang@fau.de>
2019-11-15 14:35:19 +0100

    copying the header information
    copying the information which won't differ between the source file and
    the shrinked file

--- main.c
+++ main.c
@@ -8,9 +8,79 @@
 #include <string.h>
 
 #include "libelf/libelf.h"
+#include "libelf/gelf.h"
 
 
+// TODO: better error messages
 const char *FILESUFFIX = ".shrinked";
+
+/*
+ * sets e_ident, e_machine, e_type, e_flags, e_ehsize, e_phentsize and
+ * e_shentsize for the new file
+ *
+ * does NOT set e_entry, e_phoff, e_shoff, e_phnum, e_shnum and e_shstrndx
+ */
+int copy_header_info(Elf *srce, Elf *dste) {
+	int elfclass;	//ELF class of source file
+	if ((elfclass = gelf_getclass(srce)) == ELFCLASSNONE) {
+		error(0, 0, "could not retrieve ELF class for source file");
+		return -1;
+	}
+	// executable header of source file
+	GElf_Ehdr *srcehdr = calloc(1, sizeof(GElf_Ehdr));
+	if(srcehdr == NULL) {
+		error(0, errno, "unable to allocate memory for executable header of source file");
+		return -1;
+	}
+	if (gelf_getehdr(srce, srcehdr) == NULL) {
+		error(0, 0, "gelf_getehdr() failed: %s", elf_errmsg(-1));
+		return -1;
+	}
+	// executable header of new file
+	GElf_Ehdr *dstehdr = calloc(1, sizeof(GElf_Ehdr));
+	if(dstehdr == NULL) {
+		error(0, errno, "unable to allocate memory for executable header of new file");
+		goto err_free_srcehdr;
+	}
+	/*
+	 * gelf_newehdr sets automatically the magic numbers of an ELF header,
+	 * the EI_CLASS byte according to elfclass, the EI_VERSION byte and
+	 * e_version to the version you told the library to use.
+	 *
+	 * The EI_DATA byte is set to ELFDATANONE, e_machine to EM_NONE and
+	 * e_type to ELF_K_NONE.
+	 *
+	 * Other members are set to zero. This includes the EI_OSABI and
+	 * EI_ABIVERSION bytes.
+	 */
+	if ((dstehdr = gelf_newehdr(dste, elfclass)) == NULL) {
+		error(0, 0, "gelf_newehdr() failed: %s", elf_errmsg(-1));
+		goto err_free_dstehdr;
+	}
+	dstehdr->e_ident[EI_DATA] = srcehdr->e_ident[EI_DATA];
+	dstehdr->e_machine = srcehdr->e_machine;
+	dstehdr->e_type = srcehdr->e_type;
+	dstehdr->e_phentsize = srcehdr->e_phentsize;	// FIXME: check why it's 0
+	dstehdr->e_shentsize = srcehdr->e_shentsize;
+	dstehdr->e_ehsize = srcehdr->e_ehsize;
+	dstehdr->e_flags = srcehdr->e_flags;
+	// TODO: comment what elf_update does to executable headers
+	// nothing. watch out for broken ELF headers!
+	if (elf_update(dste, ELF_C_NULL) == -1) {
+		error(0, 0, "elf_update(NULL) failed: %s", elf_errmsg(-1));
+		goto err_free_dstehdr;
+	}
+
+	free(dstehdr);
+	free(srcehdr);
+	return 0;
+
+err_free_dstehdr:
+	free(dstehdr);
+err_free_srcehdr:
+	free(srcehdr);
+	return -1;
+}
 
 int main(int argc, char **argv) {
 	// TODO: better command line parsing
@@ -57,6 +127,10 @@ int main(int argc, char **argv) {
 		goto err_free_dstfd;
 	}
 
+	if (copy_header_info(srce, dste) != 0)
+		goto err_free_dste;
+
+	// tidy up
 	elf_end(dste);
 	if (close(dstfd) < 0) {
 		error(0, errno, "close %s failed", dstfname);
@@ -69,6 +143,8 @@ int main(int argc, char **argv) {
 
 	return 0;
 
+err_free_dste:
+	elf_end(dste);
 err_free_dstfd:
 	if (close(dstfd) < 0)
 		error(0, errno, "close %s failed", dstfname);
