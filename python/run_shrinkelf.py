f6f77d55487d5475bd2f1fb939b2521b9a818d4e
Andreas Ziegler <andreas.ziegler@fau.de>
2021-09-06 12:11:43 +0200

    run_shrinkelf: add wrapper script to automate runtime measurements
    With this script, we can directly shrink all libraries in a
    given LibraryStore after elfremove has rewritten them and the
    respective parameter lists have been generated by
    'print_info.py -l --libonly --shrinkelf_params <json>'.

---
+++ python/run_shrinkelf.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+import time
+
+from librarytrader.librarystore import LibraryStore
+
+shrinkelf = '{}/shrinkelf.py'.format(os.path.dirname(sys.argv[0]))
+solver = ''
+solver_parameters = []
+if solver in ('gurobi', 'z3'):
+    solver_parameters = ['-p', solver]
+
+print('Usage: {} <LibraryStore file> <output directory>'.format(os.path.basename(sys.argv[0])))
+store_path = sys.argv[1]
+output_dir = sys.argv[2]
+if len(sys.argv) > 3:
+    solver = sys.argv[3]
+
+store = LibraryStore()
+store.load(store_path)
+# Create output directory if it does not exist
+os.makedirs(output_dir, exist_ok=True)
+
+tailor_dir = 'tailored_libs_{}/'.format(store_path)
+stats_file = '{}/stats.csv'.format(tailor_dir)
+run_times = {}
+
+for library in store.get_library_objects():
+    file_to_tailor = '{}/{}'.format(tailor_dir, library.fullname)
+    if not os.path.exists(file_to_tailor):
+        continue
+
+    # Output to <output_dir>/<basename>
+    bname = os.path.basename(file_to_tailor)
+    out_file = os.path.join(output_dir, bname)
+
+    parameter_file = 'func_offset_parameters_{}'.format(bname)
+    parameter_list = []
+    with open(parameter_file, 'r') as infd:
+        for line in infd:
+            parameter_list.extend(line.strip().split())
+
+    command_list = [shrinkelf, *parameter_list, '-o', out_file, *solver_parameters, file_to_tailor]
+    print('Running shrinkelf on {}'.format(bname))
+    time_before = time.time()
+    res = subprocess.run(command_list)
+    if res.returncode != 0:
+        print("Error for {}".format(bname))
+
+    running_time = time.time() - time_before
+
+    run_times[out_file] = running_time
+
+with open(stats_file, 'r') as fd:
+    lines = [l.strip() for l in fd.readlines()]
+    for idx, line in enumerate(lines):
+        if idx == 0:
+            lines[0] += ',filesize after,shrinkelf time'
+            continue
+
+        filename = os.path.basename(line.split(',')[0])
+        full_path = os.path.join(output_dir, filename)
+        if os.path.isfile(full_path):
+            lines[idx] += ',{},{}'.format(os.stat(full_path).st_size,
+                                          run_times[full_path])
+        else:
+            print('WARNING: no shrunk file for {}'.format(filename))
+            lines[idx] += ',{},{}'.format(0, 0)
+
+    with open(os.path.join(os.path.dirname(stats_file), 'stats_full.csv'), 'w') as outfd:
+        for line in lines:
+            outfd.write(line + '\n')
