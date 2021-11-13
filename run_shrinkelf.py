rupran/shrinkelf

commit 918d34e3e7d88da443acb63fb7f2be542a7f4f85

run_shrinkelf.py: keep original directory tree structure for shrunk files

run_shrinkelf.py
@@ -51,13 +51,14 @@ for library in store.get_library_objects():
     if not os.path.exists(file_to_tailor):
         continue
 
-    # Output to <output_dir>/<basename>
+    # Output to <output_dir>/<fullname>
     bname = os.path.basename(file_to_tailor)
-    out_file = os.path.join(output_dir, bname)
+    out_path = os.path.join(output_dir, library.fullname[1:])
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
 
     keep_file = 'keep_file_{}'.format(bname)
 
-    command_list = [shrinkelf, '-K', keep_file, '-o', out_file,
+    command_list = [shrinkelf, '-K', keep_file, '-o', out_path,
                     *solver_parameters, file_to_tailor]
     print('Running shrinkelf on {}'.format(bname))
     time_before = time.time()
@@ -67,7 +68,7 @@ for library in store.get_library_objects():
 
     running_time = time.time() - time_before
 
-    run_times[out_file] = running_time
+    run_times[library.fullname] = running_time
 
 with open(stats_file, 'r') as fd:
     lines = [l.strip() for l in fd.readlines()]
@@ -76,7 +77,7 @@ with open(stats_file, 'r') as fd:
             lines[0] += ',filesize after,shrinkelf time'
             continue
 
-        filename = os.path.basename(line.split(',')[0])
+        filename = line.split(',')[0]
         full_path = os.path.join(output_dir, filename)
         if os.path.isfile(full_path):
             if full_path not in run_times:

commit d4f2d88529e7d382678a502d6c2aad5409ef8263

run_shrinkelf.py: fix paths to get actual filesize after shrinking
Python pitfall: os.path.join(p1, p2) returns just p2 if p2 is already
an absolute path. To make an actual joined path, we need to strip
away the leading dash to make it a relative path.

Fixes: 918d34e3e7d8 ("run_shrinkelf.py: keep original directory tree
structure for shrunk files")

run_shrinkelf.py
@@ -53,7 +53,7 @@ for library in store.get_library_objects():
 
     # Output to <output_dir>/<fullname>
     bname = os.path.basename(file_to_tailor)
-    out_path = os.path.join(output_dir, library.fullname[1:])
+    out_path = os.path.join(output_dir, library.fullname.lstrip('/'))
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
 
     keep_file = 'keep_file_{}'.format(bname)
@@ -78,15 +78,15 @@ with open(stats_file, 'r') as fd:
             continue
 
         filename = line.split(',')[0]
-        full_path = os.path.join(output_dir, filename)
+        full_path = os.path.join(output_dir, filename.lstrip('/'))
         if os.path.isfile(full_path):
-            if full_path not in run_times:
+            if filename not in run_times:
                 print('WARNING: no run times for {}'.format(filename))
                 lines[idx] += ',0,0'
                 continue
             else:
                 lines[idx] += ',{},{}'.format(os.stat(full_path).st_size,
-                                            run_times[full_path])
+                                              run_times[filename])
         else:
             print('WARNING: no shrunk file for {}'.format(filename))
             lines[idx] += ',{},{}'.format(0, 0)